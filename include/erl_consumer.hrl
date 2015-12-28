a8eebfd33edc5154633d2030be53ee19928cc9c3
WanliTian <buaatianwanli@gmail.com>
2015-12-28 21:13:25 +0800

    init connection

--- include/erl_consumer.hrl
+++ include/erl_consumer.hrl
@@ -1,5 +1,20 @@
+
+-record(location, {
+    host :: inet:ip_address() | inet:hostname(),
+    port :: inet:port_number(),
+    ref  :: gen_tcp:socket()
+}).
+
 -record(anchor, {
-    consumer_group :: binary(),
-    topic          :: binary(),
-    partition      :: integer()
+    group_id  :: binary(),
+    topic     :: binary(),
+    partition :: integer()
+}).
+
+-record(conn_state,{
+    bro           :: #location{},
+    coo           :: #location{}
+    anchor        :: #anchor{},
+    offset        :: integer(),
+    is_down=false :: boolean()
 }).
--- src/connection.erl
+++ src/connection.erl
@@ -1,12 +1,16 @@
 -module(connection).
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
+-define(TCP_OPTS, [binary, {active, true}, {packet, 4}]).
+
+-include("erl_consumer.hrl").
+-include("protocol.hrl").
 
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -19,15 +23,26 @@
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Args) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, Args, []).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init({Host, Port, GroupId, Topic, Partition}) ->
+    State = #conn_state{
+        bro=#location{
+            host = Host,
+            port = Port
+        },
+        anchor=#anchor{
+            group_id = GroupId,
+            topic=Topic,
+            partition=Partition
+        }
+    },
+    {ok, State, 0}.
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
@@ -35,6 +50,16 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info(timeout, State) ->
+    {noreply, State};
+handle_info({tcp, _Ref, _Packet}, #conn_state{bro=#location{ref=_Ref}}=State) ->
+    {noreply, State};
+handle_info({tcp, _Ref, _Packet}, #conn_state{coor=#location{ref=_Ref}}=State) ->
+    {noreply, State};
+handle_info({tcp_closed, _Ref}, #conn_state{bro=#location{ref=_Ref}}=State) ->
+    {noreply, State};
+handle_info({tcp_closed, _Ref}, #conn_state{coor=#location{ref=_Ref}}=State) ->
+    {noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -47,4 +72,13 @@ code_change(_OldVsn, State, _Extra) ->
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+connect_to_bro(State=#conn_state{bro=Bro=#localtion{host=Host,port=Port}})->
+    {ok, Ref} = gen_tcp:connect(Host, Port, ?TCP_OPTS),
+    NewBro=Bro#location{ref=Ref},
+    State#conn_state{bro=NewBro}.
 
+connect_to_coor(State) ->
+    ok.
+
+prepare_offset(State) ->
+    ok.
